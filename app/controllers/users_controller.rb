5aab7db816acdb694d9ba42b37d836fb8659a5f6
Florian Lahitte <florianlahitte@MacBook-Air-de-Florian.local>
2016-09-19 12:06:45 +0200

    first commit and Add Devise and Paperclip

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,24 @@
+class UsersController < ApplicationController
+
+  def create
+    @user = User.create( user_params )
+  end
+
+  def update
+    @user = User.find(params[:id])
+    if @user.update(params[:user].permit( user_params )
+      redirect_to @pages
+    else
+      render 'edit'
+    end
+  end
+  private
+
+  # Use strong_parameters for attribute whitelisting
+  # Be sure to update your create() and update() controller methods.
+
+  def user_params
+    params.require(:user).permit(:email, :avatar)
+  end
+
+end
