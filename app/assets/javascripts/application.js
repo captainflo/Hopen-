de30c7e5d26a52fde46447733a4bc9d288b3d07f
Florian Lahitte <florianlahitte@MacBook-Air-de-Florian.local>
2016-09-29 16:10:05 +0200

    Add animation heart

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -17,7 +17,7 @@
 //= require turbolinks
 //= require_tree .
 
-$(document).ready(function() {
+$(document).on('turbolinks:load', function() {
 
   // activer les liens internes, précédé par un #
   $('a[href^="#"]').click(function(event) {
